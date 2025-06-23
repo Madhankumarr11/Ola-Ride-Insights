7af00a222935bf57bdac9e80f48543b32f52f4e6
Madhankumarr11 <srmadhan95@gmail.com>
2025-06-24 00:25:39 +0530

    Make code

---
+++ Checking_Code.py
@@ -0,0 +1,7 @@
+#Checking_Code
+
+
+# print(ola.isnull().sum())
+# # print(ola.dtypes)  #Data Types Check 
+# print(ola.shape)       # Total rows & columns
+# print(ola.info())      # Structure of dataset
