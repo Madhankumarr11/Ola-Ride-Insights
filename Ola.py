b6d55a8abce09458d05f11b902f673e244993663
Madhankumarr11 <srmadhan95@gmail.com>
2025-08-20 23:51:47 +0530

    Data celaning Start

--- Ola.py
+++ Ola.py
@@ -2,7 +2,15 @@ import pandas as pd
 
 ola = pd.read_excel('D:/SRMK/Guvi/Ola-Ride-Insights/OLA_DataSet.xlsx')
 
-print("shape", ola.shape)
+# print("shape", ola.shape)
+
+# print(ola.info())
+
+# print(ola.describe())
+
+# print(ola.isnull().sum())
+
+print((ola.isnull().sum() / len(ola))*100)
 
 
 
